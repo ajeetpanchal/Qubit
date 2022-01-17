cf64028ef1dc213d13a994f72a6bfa21630207b4
Ajeet Panchal <ajeetjpanchal@gmail.com>
2022-01-17 22:32:55 +0530

    navbar frontend and create post and frontend of the feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from './components/login/Login';
 import { BrowserRouter } from "react-router-dom";
-
+import Navbar from './components/login/Navbar';
+import Post from './components/login/Post';
+import Setting from './components/login/Settings';
 function App() {
   return (
     <BrowserRouter>
-      <Login />
+    <Login/>
     </BrowserRouter>
   );
 }
