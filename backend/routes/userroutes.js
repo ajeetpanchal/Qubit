be760f92fb221444627e6ae0861b7ff3c59c96f1
Aastha0202 <aasthamodi22@gmail.com>
2022-01-24 22:01:31 +0530

    JWT TOKEN and chatpage backend added

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,9 +1,16 @@
+//for all routes of our website
+
 const express = require("express");
 const router = express.Router();
 const connctDB = require("../config/db");
+const Chat = require("../models/chatschema");
 const Logins = require("../models/userschema");
+const Messages = require("../models/messageSchema");
+const generateToken = require("../config/generatetoken");
+const protect = require("../middleware/authmiddleware");
+
 connctDB();
-//login routes
+//login routes to login into the website
 router.post("/login", async (req, res) => {
   // res.json({ message: req.body });
   try {
@@ -23,12 +30,25 @@ router.post("/login", async (req, res) => {
     if (!userLogin) {
       res.status("400").json({ error: "please enter valid data" });
     } else {
-      res.json({ message: "user login suceessfully" });
+      res.status(200);
+      const token = generateToken(userLogin._id);
+      res.send({
+        _id: userLogin._id,
+        College_id: userLogin.College_id,
+        College_name: userLogin.College_name,
+        name: userLogin.name,
+        email: userLogin.email,
+        pic: userLogin.pic,
+        token: token,
+      });
+      res.json({ message: "login sucessfully" });
     }
   } catch (e) {
     console.error("some error accure ");
   }
 });
+
+//reset password routes if user forgot the password
 
 router.post("/forgot", async (req, res) => {
   try {
@@ -60,6 +80,93 @@ router.post("/forgot", async (req, res) => {
     }
   } catch (e) {
     console.log("some issue is there ");
+  }
+});
+
+//to search the users of our website
+//it also uses the protect middleware so search result will not return currently loggin user.
+router.get("/message", protect, async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const userid = await Logins.find(keyword).find({
+    _id: { $ne: req.message._id },
+  });
+  console.log(userid);
+});
+
+//create one to one chat and check the user who is craeting the chat is valid authorize user or not
+//alse check other user whith whome the chat will create that user is also valid authorize or not
+router.post("/chat", protect, async (req, res) => {
+  //create one on one chat
+  const { userId } = req.body;
+
+  if (!userId) {
+    console.log("UserId param not sent with request");
+    return res.sendStatus(400);
+  }
+
+  var isChat = await Chat.find({
+    $and: [
+      { users: { $elemMatch: { $eq: req.message._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password", Logins)
+    .populate("latestMessage", Messages);
+
+  isChat = await Logins.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+  } else {
+    var chatData = {
+      chatName: "sender",
+      users: [req.message._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password",
+        Logins
+      );
+      res.status(200).json(FullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
+});
+//fetch all the chats
+router.get("/chat", protect, async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.message._id } } })
+      .populate("users", "-password", Logins)
+      .populate("latestMessage", Messages)
+      .sort({
+        updateAt: -1,
+      })
+      .then(async (results) => {
+        results = await Logins.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (e) {
+    res.status(400);
+    console.error(e);
   }
 });
 
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Forgotpassword from "./Forgotpassword";
 import Userlogin from "./Userlogin";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
 
 export default function Login() {
   return (
     <Routes>
       <Route path="/" element={<Userlogin />} />
       <Route path="/forgot" element={<Forgotpassword />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
